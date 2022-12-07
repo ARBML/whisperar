2e44e3e209a745d0c40d8dbc05c61cabab93caff
Zaid Alyafeai <alyafey22@gmail.com>
2022-12-08 02:25:52 +0300

    Update setup_libs.sh

--- setup_libs.sh
+++ setup_libs.sh
@@ -7,4 +7,5 @@ huggingface-cli repo create $2
 git clone https://huggingface.co/$1/$2
 cd $2
 cp ../run_speech_recognition_seq2seq_streaming.py .
+cp ../run_speech_recognition_seq2seq.py .
 cp ../run.sh .
