bf9fbe7fd5a87d6d7ddc5daf429574cea4626faf
zaidalyafeai <alyafey22@gmail.com>
2022-12-15 12:49:38 +0300

    scripts for evaluating on fleurs and common voice 11

---
+++ run_eval_cv_11.sh
@@ -0,0 +1,10 @@
+!python run_eval_whisper_streaming.py \
+        --dataset mozilla-foundation/common_voice_11_0 \
+        --model_id $1 \
+        --split test \
+        --config ar_eg \
+        --batch_size="16" \
+        --remove_diacritics="True" \
+        --language="ar" \
+        --device 0 \
+        --streaming="False"
---
+++ run_eval_fleurs.sh
@@ -0,0 +1,10 @@
+!python run_eval_whisper_streaming.py \
+        --dataset google/fleurs \
+        --model_id $1 \
+        --split test \
+        --config ar_eg \
+        --batch_size="16" \
+        --remove_diacritics="True" \
+        --language="ar" \
+        --device 0 \
+        --streaming="False"
