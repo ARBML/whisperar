7f4152d49762052f544a12e977fc6c21a6308dab
zaidalyafeai <alyafey22@gmail.com>
2022-12-15 14:04:30 +0300

    fix split of cv

--- run_eval_cv_11.sh
+++ run_eval_cv_11.sh
@@ -2,7 +2,7 @@ python run_eval_whisper_streaming.py \
         --dataset mozilla-foundation/common_voice_11_0 \
         --model_id $1 \
         --split test \
-        --config ar_eg \
+        --config="ar" \
         --batch_size="16" \
         --remove_diacritics="True" \
         --language="ar" \
